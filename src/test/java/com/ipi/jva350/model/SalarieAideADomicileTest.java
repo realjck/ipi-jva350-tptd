b0815a5536259ef1b7c8414c2de7e64ed9abe441
jck <jckleinbourg@gmail.com>
2024-02-22 14:56:40 +0100

    feat: Ajoute test paramétré pour la vérification du décompte de congés payés par plages de dates

--- src/test/java/com/ipi/jva350/model/SalarieAideADomicileTest.java
+++ src/test/java/com/ipi/jva350/model/SalarieAideADomicileTest.java
@@ -2,12 +2,15 @@ package com.ipi.jva350.model;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.LocalDate;
+import java.util.LinkedHashSet;
 
-public class SalarieAideADomicileTest {
+class SalarieAideADomicileTest {
     @Test
-    public void  testALegalementDroitADesCongesPayesTrue() {
+    void testALegalementDroitADesCongesPayesTrue() {
         // Given
         SalarieAideADomicile monSalarie = new SalarieAideADomicile(
                 "Robertus",
@@ -25,7 +28,7 @@ public class SalarieAideADomicileTest {
         Assertions.assertEquals(true, result, "Teste ");
     }
     @Test
-    public void  testALegalementDroitADesCongesPayesFalse() {
+    void testALegalementDroitADesCongesPayesFalse() {
         // Given
         SalarieAideADomicile monSalarie = new SalarieAideADomicile(
                 "Robertus",
@@ -43,7 +46,7 @@ public class SalarieAideADomicileTest {
         Assertions.assertEquals(false, result);
     }
     @Test
-    public void testEstHabituellementTravailleTrue() {
+    void testEstHabituellementTravailleTrue() {
         // Given
         SalarieAideADomicile salarie = new SalarieAideADomicile();
         LocalDate jour = LocalDate.of(2024, 2,19);
@@ -53,7 +56,7 @@ public class SalarieAideADomicileTest {
         Assertions.assertEquals(true, result);
     }
     @Test
-    public void testEstHabituellementTravailleFalse() {
+     void testEstHabituellementTravailleFalse() {
         // Given
         SalarieAideADomicile salarie = new SalarieAideADomicile();
         LocalDate jour = LocalDate.of(2024, 2,18);
@@ -61,5 +64,31 @@ public class SalarieAideADomicileTest {
         boolean result = salarie.estHabituellementTravaille(jour);
         // Then
         Assertions.assertEquals(false, result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'2023-12-17', '2023-12-28', 9",
+            "'2023-12-17', '2024-01-08', 17"
+    })
+    void calculeJoursDeCongeDecomptesPourPlage(String dateDebut, String dateFin, int expectedResult){
+        // Given
+        SalarieAideADomicile salarie = new SalarieAideADomicile(
+                "Robert",
+                LocalDate.of(2023,6,28),
+                LocalDate.now(),
+                20,
+                2.5,
+                9,
+                1,
+                8
+        );
+        // When
+        LinkedHashSet<LocalDate> result = salarie.calculeJoursDeCongeDecomptesPourPlage(
+                LocalDate.parse(dateDebut),
+                LocalDate.parse(dateFin)
+        );
+        // Then
+        Assertions.assertEquals(expectedResult, result.size());
     }
 }
