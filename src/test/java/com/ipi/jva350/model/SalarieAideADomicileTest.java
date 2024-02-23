be1e8f7605489044d591ad05c71da283945be8ef
jck <jckleinbourg@gmail.com>
2024-02-23 16:19:36 +0100

    chore: Corrige code-smells avec Sonar

--- src/test/java/com/ipi/jva350/model/SalarieAideADomicileTest.java
+++ src/test/java/com/ipi/jva350/model/SalarieAideADomicileTest.java
@@ -8,52 +8,58 @@ import org.junit.jupiter.params.provider.CsvSource;
 import java.time.LocalDate;
 import java.util.LinkedHashSet;
 
-public class SalarieAideADomicileTest {
+class SalarieAideADomicileTest {
 
     @Test
-    public void testALegalementDroitADesCongesPayesDefaultValue() {
+    void testALegalementDroitADesCongesPayesDefaultValue() {
         // Given :
         SalarieAideADomicile monSalarie = new SalarieAideADomicile();
         // When :
         boolean res = monSalarie.aLegalementDroitADesCongesPayes();
         // Then :
-        Assertions.assertEquals(false, res);
+        Assertions.assertFalse(res);
     }
 
     @Test
-    public void testALegalementDroitADesCongesPayesNominal() {
+    void testALegalementDroitADesCongesPayesNominal() {
         // Given :
-        SalarieAideADomicile monSalarie = new SalarieAideADomicile("Paul",
-                LocalDate.of(2023, 6, 28), LocalDate.now(), 20, 2.5,
-                120, 15, 8);
+        SalarieAideADomicile monSalarie = new SalarieAideADomicile(
+                "Paul",
+                LocalDate.of(2023, 6, 28), LocalDate.now(),
+                20, 2.5,
+                120, 15, 8
+        );
         // When :
-        boolean res = monSalarie.aLegalementDroitADesCongesPayes();
+        boolean result = monSalarie.aLegalementDroitADesCongesPayes();
         // Then :
-        Assertions.assertEquals(true, res);
+        Assertions.assertTrue(result);
     }
 
     @Test
-    public void testALegalementDroitADesCongesPayesTrue() {
+    void testALegalementDroitADesCongesPayesTrue() {
         // Given :
         SalarieAideADomicile monSalarie = new SalarieAideADomicile("Paul",
                 LocalDate.of(2023, 6, 28), LocalDate.now(), 20, 2.5,
                 10, 1, 8);
         // When :
-        boolean res = monSalarie.aLegalementDroitADesCongesPayes();
+        boolean result = monSalarie.aLegalementDroitADesCongesPayes();
         // Then :
-        Assertions.assertEquals(true, res, "avec 10 jours travaillés en N-1 (au moins), le résultat doit être vrai");
+        Assertions.assertTrue(result, "avec 10 jours travaillés en N-1 (au moins), le résultat doit être vrai");
     }
 
     @Test
-    public void testALegalementDroitADesCongesPayesFalse() {
+    void testALegalementDroitADesCongesPayesFalse() {
         // Given :
-        SalarieAideADomicile monSalarie = new SalarieAideADomicile("Paul",
-                LocalDate.of(2023, 6, 28), LocalDate.now(), 20, 2.5,
-                9, 1, 8);
+        SalarieAideADomicile monSalarie = new SalarieAideADomicile(
+                "Paul",
+                LocalDate.of(2023, 6, 28), LocalDate.now(),
+                20, 2.5,
+                9, 1, 8
+        );
         // When :
-        boolean res = monSalarie.aLegalementDroitADesCongesPayes();
+        boolean result = monSalarie.aLegalementDroitADesCongesPayes();
         // Then :
-        Assertions.assertEquals(false, res, "avec 9 jours travaillés en N-1 (au plus), le résultat doit être faux");
+        Assertions.assertFalse(result, "avec 9 jours travaillés en N-1 (au plus), le résultat doit être faux");
     }
 
     @ParameterizedTest
@@ -64,11 +70,14 @@ public class SalarieAideADomicileTest {
             "'2023-12-17', '2023-12-31', 11",
             "'2023-12-17', '2024-01-08', 17"
     })
-    public void testCalculeJoursDeCongeDecomptesPourPlage(String dateDebut, String dateFin, int expectedNb) {
+    void testCalculeJoursDeCongeDecomptesPourPlage(String dateDebut, String dateFin, int expectedNb) {
         // Given :
-        SalarieAideADomicile monSalarie = new SalarieAideADomicile("Paul",
-                LocalDate.of(2023, 6, 28), LocalDate.now(), 20, 2.5,
-                9, 1, 8);
+        SalarieAideADomicile monSalarie = new SalarieAideADomicile(
+                "Paul",
+                LocalDate.of(2023, 6, 28),
+                LocalDate.now(), 20, 2.5,
+                9, 1, 8
+        );
         // When :
         LinkedHashSet<LocalDate> resNb = monSalarie.calculeJoursDeCongeDecomptesPourPlage(
                 LocalDate.parse(dateDebut),
