d4f1eef5c33832f0d2e8c613caf456085ba96264
jck <jckleinbourg@gmail.com>
2024-03-05 00:48:34 +0100

    feat: Intègre tests salarié (par Alvin Kita)

--- src/test/java/com/ipi/jva350/model/SalarieAideADomicileTest.java
+++ src/test/java/com/ipi/jva350/model/SalarieAideADomicileTest.java
@@ -1,6 +1,7 @@
 package com.ipi.jva350.model;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -9,81 +10,88 @@ import java.time.LocalDate;
 import java.util.LinkedHashSet;
 
 class SalarieAideADomicileTest {
+    SalarieAideADomicile salarieTest;
 
-    @Test
-    void testALegalementDroitADesCongesPayesDefaultValue() {
-        // Given :
-        SalarieAideADomicile monSalarie = new SalarieAideADomicile();
-        // When :
-        boolean res = monSalarie.aLegalementDroitADesCongesPayes();
-        // Then :
-        Assertions.assertFalse(res);
-    }
 
-    @Test
-    void testALegalementDroitADesCongesPayesNominal() {
-        // Given :
-        SalarieAideADomicile monSalarie = new SalarieAideADomicile(
-                "Paul",
-                LocalDate.of(2023, 6, 28), LocalDate.now(),
-                20, 2.5,
-                120, 15, 8
+    /**
+     * Salarié à initialiser avant chaques tests
+     */
+    @BeforeEach
+    public void setUpSalarie() {
+        salarieTest = new SalarieAideADomicile(
+                "toto",
+                LocalDate.of(2023,6,28),
+                LocalDate.now(),
+                20,
+                2.5,
+                10,
+                15,
+                8
         );
-        // When :
-        boolean result = monSalarie.aLegalementDroitADesCongesPayes();
-        // Then :
-        Assertions.assertTrue(result);
     }
 
+
+    /**
+     * Test de la méthode ALégalementDroitADesCongésPayes().
+     * D'après la documentation les critères d'admissions sont :
+     *    - Avoir travaillé pendant au moins 10 jours (Pas forcément de suite)
+     *    - Avoir un jour de congé N-1
+     */
     @Test
     void testALegalementDroitADesCongesPayesTrue() {
-        // Given :
-        SalarieAideADomicile monSalarie = new SalarieAideADomicile("Paul",
-                LocalDate.of(2023, 6, 28), LocalDate.now(), 20, 2.5,
-                10, 1, 8);
-        // When :
-        boolean result = monSalarie.aLegalementDroitADesCongesPayes();
-        // Then :
-        Assertions.assertTrue(result, "avec 10 jours travaillés en N-1 (au moins), le résultat doit être vrai");
+        // Given : Mise en place de l'environnement de test
+        // ⇾ Le salarie est initialisé avec le @BeforeEach
+
+        // When : Test
+        boolean auMoinsDixJoursTravaille = salarieTest.aLegalementDroitADesCongesPayes(); // True si nbCongés >= 10
+        boolean auMoinsUnJoursDansLeSoldeConges = salarieTest.getCongesPayesRestantAnneeNMoins1() > 0; // True si au moins 1 jour de congé
+
+        // Then : Vérifie que le salarié a le droit à des congés payés
+        Assertions.assertTrue(auMoinsDixJoursTravaille, "Le nombre de jours travaillé \"" + salarieTest.getJoursTravaillesAnneeNMoins1() + " jours\" n'est pas supérieur ou égal à 10.");
+        Assertions.assertTrue(auMoinsUnJoursDansLeSoldeConges, "Aucun jours de congés dans le solde N-1.");
     }
 
+    /**
+     * Test de la méthode estHabituellementTravaille() à partir d'une date donnée
+     */
     @Test
-    void testALegalementDroitADesCongesPayesFalse() {
-        // Given :
-        SalarieAideADomicile monSalarie = new SalarieAideADomicile(
-                "Paul",
-                LocalDate.of(2023, 6, 28), LocalDate.now(),
-                20, 2.5,
-                9, 1, 8
-        );
-        // When :
-        boolean result = monSalarie.aLegalementDroitADesCongesPayes();
-        // Then :
-        Assertions.assertFalse(result, "avec 9 jours travaillés en N-1 (au plus), le résultat doit être faux");
+    void testEstHabituellementTravailleTrue() {
+        // Given : mise en place de la date à tester
+        // ⇾ Le salarie est initialisé avec le @BeforeEach
+        LocalDate dateTest = LocalDate.of(2024,2,19); // ⇾ date de test
+
+        // When : Test si le jour est habituellement travaillé
+        boolean testEstHabituellementTravailleResult = salarieTest.estHabituellementTravaille(dateTest);
+
+        // Then : Vérification
+        Assertions.assertTrue(testEstHabituellementTravailleResult, dateTest.getDayOfWeek() + " n'est pas un jours habituelement travaillé.");
     }
 
-    @ParameterizedTest
+
+    /**
+     * Test parametré de calculeJoursDeCongeDecomptesPourPlage()
+     * @param dateDebut Début de la plage de test
+     * @param dateFin Fin de la plage de test
+     * @param expectedResult Résultat attendu pour calculeJoursDeCongeDecomptesPourPlage(dateDebut, dateFin)
+     */
+    @ParameterizedTest(name = "Plage {0} à {1}, résultat attendu : {2}")
     @CsvSource({
             "'2023-12-17', '2023-12-28', 9",
-            "'2023-12-17', '2023-12-29', 11",
-            "'2023-12-17', '2023-12-30', 11",
-            "'2023-12-17', '2023-12-31', 11",
             "'2023-12-17', '2024-01-08', 17"
     })
-    void testCalculeJoursDeCongeDecomptesPourPlage(String dateDebut, String dateFin, int expectedNb) {
-        // Given :
-        SalarieAideADomicile monSalarie = new SalarieAideADomicile(
-                "Paul",
-                LocalDate.of(2023, 6, 28),
-                LocalDate.now(), 20, 2.5,
-                9, 1, 8
-        );
-        // When :
-        LinkedHashSet<LocalDate> resNb = monSalarie.calculeJoursDeCongeDecomptesPourPlage(
+    void calculeJoursDeCongeDecomptesPourPlage(String dateDebut, String dateFin, int expectedResult){
+        // Given
+        // ⇾ Le salarie est initialisé avec le @BeforeEach
+
+        // When
+        LinkedHashSet<LocalDate> result = salarieTest.calculeJoursDeCongeDecomptesPourPlage(
                 LocalDate.parse(dateDebut),
-                LocalDate.parse(dateFin));
-        // Then :
-        Assertions.assertEquals(expectedNb, resNb.size());
+                LocalDate.parse(dateFin)
+        );
+        // Then
+        Assertions.assertEquals(expectedResult, result.size());
     }
+
+
 
 }
