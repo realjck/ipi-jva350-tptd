8df1657ab5a60aa7cf001bb9752011173b73cc61
jck <jckleinbourg@gmail.com>
2024-03-07 22:43:42 +0100

    feat: Ajoute mock pour Ajout des jours de congés, corrigé pour test : nécessite 10j travaillés

--- src/test/java/com/ipi/jva350/model/SalarieAideADomicileServiceMockTest.java
+++ src/test/java/com/ipi/jva350/model/SalarieAideADomicileServiceMockTest.java
@@ -6,8 +6,6 @@ import com.ipi.jva350.service.SalarieAideADomicileService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -15,31 +13,32 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
-import java.util.LinkedHashSet;
 
 @ExtendWith(MockitoExtension.class)
-public class SalarieAideADomicileServiceMockTest {
+class SalarieAideADomicileServiceMockTest {
 
     @Mock
     private SalarieAideADomicileRepository salarieAideADomicileRepository;
     @InjectMocks
-    private SalarieAideADomicileService salarieService = new SalarieAideADomicileService();
+    private SalarieAideADomicileService salarieService;
 
     // mocké :
     @Test
-    public void testAjouteConge() throws SalarieException {
+    void testAjouteConge() throws SalarieException {
         // Given :
         SalarieAideADomicile monSalarie = new SalarieAideADomicile("Paul",
-                LocalDate.of(2022, 6, 28), LocalDate.of(2023, 11, 1),
-                9, 2.5,
-                80, 20, 8);
+                LocalDate.of(
+                        2022, 6, 28),
+                        LocalDate.of(2023, 11, 1),
+                        10, 2.5,
+                        80, 20, 8);
         // When :
-        salarieService.ajouteConge(monSalarie, LocalDate.of(2024, 12, 17),
-                LocalDate.of(2024, 12, 18));
+        salarieService.ajouteConge(monSalarie, LocalDate.of(2023, 11, 13),
+                LocalDate.of(2023, 11, 17));
         // Then :
         ArgumentCaptor<SalarieAideADomicile> salarieAideADomicileCaptor = ArgumentCaptor.forClass(SalarieAideADomicile.class);
         Mockito.verify(salarieAideADomicileRepository, Mockito.times(1)).save(salarieAideADomicileCaptor.capture()); // arg capture !
-        Assertions.assertEquals(1L, salarieAideADomicileCaptor.getValue().getCongesPayesPrisAnneeNMoins1());
+        Assertions.assertEquals(6L, salarieAideADomicileCaptor.getValue().getCongesPayesPrisAnneeNMoins1());
     }
 
 }
